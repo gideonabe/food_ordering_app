39e6c896122847d41fe7a82eb5cd2c54156e00da
Gideon Abe <gideonabe2020@gmail.com>
2025-07-23 22:47:38 +0100

    more commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,14 +26,51 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false, 
-        tabBarShowLabel: false
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: 40,
+          backgroundColor: "white",
+          shadowColor: "#1a1a1a",
+          shadowOffset: {width: 0, height: 2},
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        }
       }}
     >
       <Tabs.Screen
         name='index'
         options={{
           title: 'Home',
-          tabBarIcon: ({focused}) => <TabBarIcon title="Home" icon={images.home} focused={focused}/>
+          tabBarIcon: ({focused}) => <TabBarIcon title="Home" icon={images.home} focused={focused}/>,
+        }}
+      />
+      <Tabs.Screen
+        name='search'
+        options={{
+          title: 'Search',
+          tabBarIcon: ({focused}) => <TabBarIcon title="Search" icon={images.search} focused={focused}/>
+        }}
+      />
+      <Tabs.Screen
+        name='cart'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({focused}) => <TabBarIcon title="Cart" icon={images.bag} focused={focused}/>
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({focused}) => <TabBarIcon title="Profile" icon={images.person} focused={focused}/>
         }}
       />
     </Tabs>
